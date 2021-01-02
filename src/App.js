9a67ed3b361d9c5a44032abff63408be7b6a4d2c
shir0206 <40990488+shir0206@users.noreply.github.com>
2021-01-02 13:17:46 +0200

    Implement menu & read data from server

--- src/App.js
+++ src/App.js
@@ -4,59 +4,14 @@ import axios from "axios";
 import { useCallback, useEffect, useState } from "react";
 import { Node } from "./components/Node";
 import { Gallery } from "./components/Gallery";
-import getData from "./GetData";
 
 function App() {
-  // const [dataLevel, setDataLevel] = useState({
-  //   children: [{}],
-  //   label: "a",
-  //   type: 0,
-  // });
-
-  const [nodeLabel, setNodeLabel] = useState("");
-  const [nodeChildren, setNodeChildren] = useState([]);
-  const [nodeType, setNodeType] = useState(0);
-  const [currNode, setCurrNode] = useState("");
-
-  const [currImage, setCurrImage] = useState(null);
-  //setDataLevel(getData);
-  useEffect(() => {
-    const baseUrl = "http://18.203.83.17/public/explorePictures?path=root";
-    const props = "";
-    const receiptUrl = props ? baseUrl + "/" + props : baseUrl;
-    const clientToken = "2d4e69f4823176197ccf41caa5ee6456";
-
-    axios
-      .get(baseUrl, {
-        json: true,
-        headers: { "X-TOKEN": clientToken },
-      })
-      .then(function (response) {
-        // handle success
-        console.log(response);
-        // setDataLevel(response.data.data);
-        setNodeLabel(response.data.data.label);
-        setNodeChildren(response.data.data.children);
-        setNodeType(response.data.data.type);
-        // console.log(response);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-        return window.alert("error");
-      });
-  }, []);
-
   return (
     <div className="App">
-      <Node
-        nodeType={nodeType}
-        nodeLabel={nodeLabel}
-        currNode={currNode}
-        setCurrNode={setCurrNode}
-      ></Node>
+      <ul className="nodes">
+        <Node label={"root"} type={"root"}></Node>
+      </ul>
       <Gallery></Gallery>
-      <p>{nodeLabel}</p>
     </div>
   );
 }
--- src/components/Node.js
+++ src/components/Node.js
@@ -1,22 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import axios from "axios";
+
 import "./Node.css";
 
 export const Node = (props) => {
+  const [click, setClick] = useState(false);
+  const [currNodeType, setCurrNodeType] = useState(props.type);
+  const [currNodeLabel, setCurrNodeLabel] = useState(props.label);
+  const [currNodeChildren, setCurrNodeChildren] = useState([]);
+  const [requestError, setRequestError] = useState();
+
+  const baseUrl = "http://18.203.83.17/public/explorePictures?path=";
+  const receiptUrl = props.label ? baseUrl + "/" + props.label : baseUrl;
+  const clientToken = "2d4e69f4823176197ccf41caa5ee6456";
+
+  const getData = useCallback(async () => {
+    try {
+      const result = await axios.get(`${receiptUrl}`, {
+        json: true,
+        headers: { "X-TOKEN": clientToken },
+      });
+      console.log("inside try");
+
+      const resultData = result.data.data;
+      setCurrNodeChildren(resultData);
+      setClick(true);
+      console.log("inside try=", resultData);
+    } catch (err) {
+      setRequestError(err.message);
+      window.alert(err.message);
+    }
+  });
+
   return (
-    <button
-      className={props.type}
-      onClick={() => {
-        const lastFolder = props.currNode.split("/");
-        const length = lastFolder.length;
-
-        if (lastFolder[length - 1] !== props.nodeLabel) {
-          props.setCurrNode((currNode) => currNode + "/" + props.nodeLabel);
+    <>
+      <button
+        className={
+          props.type === 0 ? (click ? "clicked-folder" : "folder") : "picture"
         }
-
-        console.log(props.currNode);
-        if (props.nodeType === 0) return;
-        if (props.nodeType === 1) return;
-      }}
-    ></button>
+        onClick={getData}
+      >
+        {props.label}
+      </button>
+      {click && (
+        <ul className="nodes">
+          {currNodeChildren.children &&
+            currNodeChildren.children.map((nodeChild) => (
+              <Node
+                key={nodeChild.label}
+                label={currNodeLabel + "/" + nodeChild.label.toString()}
+                type={nodeChild.type}
+              ></Node>
+            ))}
+        </ul>
+      )}
+    </>
   );
 };
